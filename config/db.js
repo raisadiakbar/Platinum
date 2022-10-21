3e1937a862c78f4165e2ea28c50f5141ef85a543
raisadiakbar <muhrais791@gmail.com>
2022-10-22 08:13:41 +0900

    updated

--- config/db.js
+++ config/db.js
@@ -1,32 +1,32 @@
 require('dotenv').config();
 
 module.exports = { 
-  "development": {
-    "username": process.env.USERNAME_SECRET,
-    "password": process.env.PASSWORD_SECRET,
-    "database": process.env.DATABASE_SECRET,
-    "host": process.env.HOST_SECRET,
-    "port": process.env.PORT_SECRET,
-    "dialect": process.env.DIALECT_SECRET,
-    "logging": false
+  development: {
+    username: process.env.USERNAME_SECRET,
+    password: process.env.PASSWORD_SECRET,
+    database: process.env.DATABASE_SECRET,
+    host: process.env.HOST_SECRET,
+    port: process.env.PORT_SECRET,
+    dialect: process.env.DIALECT_SECRET,
+    logging: false
   },
-  "test": {
-    "username": process.env.USERNAME_SECRET,
-    "password": process.env.PASSWORD_SECRET,
-    "database": process.env.DATABASE_SECRET,
-    "host": process.env.HOST_SECRET,
-    "port": process.env.PORT_SECRET,
-    "dialect": process.env.DIALECT_SECRET,
-    "logging": false
+  test: {
+    username: process.env.USERNAME_SECRET,
+    password: process.env.PASSWORD_SECRET,
+    database: process.env.DATABASE_SECRET,
+    host: process.env.HOST_SECRET,
+    port: process.env.PORT_SECRET,
+    dialect: process.env.DIALECT_SECRET,
+    logging: false
   },
-  "production": {
-    "username": process.env.USERNAME_SECRET,
-    "password": process.env.PASSWORD_SECRET,
-    "database": process.env.DATABASE_SECRET,
-    "host": process.env.HOST_SECRET,
-    "port": process.env.PORT_SECRET,
-    "dialect": process.env.DIALECT_SECRET,
-    "logging": false,
+  production: {
+    username: process.env.USERNAME_SECRET,
+    password: process.env.PASSWORD_SECRET,
+    database: process.env.DATABASE_SECRET,
+    host: process.env.HOST_SECRET,
+    port: process.env.PORT_SECRET,
+    dialect: process.env.DIALECT_SECRET,
+    logging: false,
     // "dialectOptions": {
     //   "ssl": {
     //     "require": true,
