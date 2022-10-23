cb85204f63a1f50ab4e01a3cf7e98a5244c5adea
raisadiakbar <muhrais791@gmail.com>
2022-10-23 19:39:28 +0900

    updated

--- tests/Admins.test.js
+++ tests/Admins.test.js
@@ -41,20 +41,20 @@ const Upload = './files/Untitled Diagram.drawio.png';
         //     expect(typeof res.body.message).toMatch('string');
         // })
 
-        // it('POST /api/admin/register with email has been ready, response should be 400', async () => {
-        //     jest.setTimeout(5000)
-        //     const res = await request(app)
-        //         .post('/api/admin/register')
-        //         .field('name', 'mimin1')
-        //         .field('email', 'mimin1@gmail.com')
-        //         .field('password', '123456')
-        //         .attach('profile', Upload)
-        //         .set('Accept', 'application/json');
+        it('POST /api/admin/register with email has been ready, response should be 400', async () => {
+            jest.setTimeout(5000)
+            const res = await request(app)
+                .post('/api/admin/register')
+                .field('name', 'mimin1')
+                .field('email', 'mimin1@gmail.com')
+                .field('password', '123456')
+                .attach('profile', Upload)
+                .set('Accept', 'application/json');
 
-        //     expect(res.status).toBe(400);
-        //     expect(res.body).toHaveProperty('error.message');
-        //     expect(typeof res.body.message).toBe('undefined');
-        // })
+            expect(res.status).toBe(400);
+            expect(res.body).toHaveProperty('error.message');
+            expect(typeof res.body.message).toBe('undefined');
+        })
 
         it('POST /api/admin/register without password, response should be 400', async () => {
             const res = await request(app) 
